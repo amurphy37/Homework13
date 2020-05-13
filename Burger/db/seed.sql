24cc27d1a2b9d7795c48f35d98f4cb4c6cdab1d6
Andrew Murphy <amurphy37@gmail.com>
2020-05-13 19:33:09 -0400

    v 1.0

---
+++ Burger/db/seed.sql
@@ -0,0 +1,12 @@
+use burgers_db;
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES 
+    ("BBQ Chicken", true),
+    ("Southwestern", false),
+    ("Bacon Cheeseburger", true),
+    ("Mesclun Around", false),
+    ("Hummus a Tune Burger", true),
+    ("Chard to a Crisp Burger", true),
+    ("Cheeses Is Born Burger", false);
