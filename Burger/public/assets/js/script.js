60bff701ac8a5556bf877d45d90a8e14dd219019
Andrew Murphy <amurphy37@gmail.com>
2020-05-15 00:31:10 -0400

    final verison

--- Burger/public/assets/js/script.js
+++ Burger/public/assets/js/script.js
@@ -2,5 +2,33 @@ $(document).ready(function() {
     
   $(".devour-form").on("submit", function(event) {
     //hint:setting up ajax put
+    event.preventDefault();
+
+    var id = $(".burger_id").val()
+
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT"
+    }).then(function () {
+      console.log("Successfully devoured");
+      location.reload();
+    });
+  });
+
+  $(".create-form").on("submit", function(event){
+  event.preventDefault();
+
+  var newBurger = {
+    name: $("#name").val().trim()
+  }
+
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function () {
+        console.log("created new burger")
+        location.reload();
+      }
+    );
   });
 });
